9f653618855fdd460dd2a4c21570dd9714101557
Ricardinntlink <ricardo.gomez@ntlink.com.mx>
2025-03-04 13:46:14 -0600

    inicio
    Se subió por primera vez

---
+++ TimbraNtlink4/Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TimbraNtlink4.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Timbrar( string archivo)
+        {
+            archivo = archivo.Replace("data:text/xml;base64,", "");
+            byte[] binaryData = Convert.FromBase64String(archivo);
+            string cfdi = System.Text.Encoding.UTF8.GetString(binaryData);
+
+            ServiceReference1.ServicioTimbradoClient cliente = new ServiceReference1.ServicioTimbradoClient();
+             var result= await cliente.TimbraCfdiSinSelloAsync("URE180429TM6@ntlink.com.mx", "NTPruebas.2021*?", cfdi);
+            if (result != null)
+            {
+                //return Content(result);
+                return Json(new { codigo = "OK", data = result }, JsonRequestBehavior.AllowGet);
+
+            }
+            else
+                return Json(new { codigo = "Error", data = result }, JsonRequestBehavior.AllowGet);
+
+
+
+        }
+    }
+}
---
+++ TimbraNtlink4/Scripts/Timbrar.js
@@ -0,0 +1,66 @@
+﻿$(document).ready(function () {
+    $('input[type=file]').change(function () {
+        var t = $(this).val();
+        var labelText = t.substr(12, t.length);
+        //$(this).prev('label').text(labelText);
+        $('#xml-name').val(labelText);
+
+    })
+
+
+});
+
+$("#xml-file").change(function (e) {
+    $("#file_error").text('');
+
+    var file = e.target.files[0];
+    // fileNameLogo = e.target.files[0].name;
+    var reader = new FileReader();
+    reader.onload = function (e) {
+        $('#hfArchivo').val(e.target.result);
+    }
+    if (file) {
+        reader.readAsDataURL(file);
+    }
+});
+
+function TimbrarCFDI() {
+
+    var base64 = $("#hfArchivo").val();
+    if (base64 == '') {
+        $("#file_error").text('seleccione un archivo XML');
+        return;
+    }
+    var request = {
+        
+        archivo: base64
+       }
+    var url = $("#RedirectTo").val();
+
+    jQuery.ajax({
+        url: url,
+        type: "POST",
+        data: JSON.stringify(request),
+        dataType: "json",
+        contentType: "application/json; charset=utf-8",
+        success: function (data) {
+
+            if (data.codigo == "OK") {
+
+                $("#txtSalida").val(data.data);
+
+
+            } else {
+                
+                $("#file_error").text(data.data);
+            }
+        },
+        error: function (error) {
+            console.log(error)
+        },
+        beforeSend: function () {
+
+        },
+    });
+
+}
